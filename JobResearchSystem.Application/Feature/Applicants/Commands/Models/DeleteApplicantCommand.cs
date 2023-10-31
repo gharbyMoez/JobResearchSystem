7a8d6561196853ee4283e0536b0dd01a241e1b23
MarwaSameer <marwasamir94@gmail.com>
2023-10-31 23:00:16 +0200

    Applicant

---
+++ JobResearchSystem.Application/Feature/Applicants/Commands/Models/DeleteApplicantCommand.cs
@@ -0,0 +1,10 @@
+﻿using JobResearchSystem.Application.Bases;
+using MediatR;
+
+namespace JobResearchSystem.Application.Feature.Applicants.Commands.Models
+{
+    public class DeleteApplicantCommand : IRequest<Response<string>>
+    {
+        public int ApplicantId { get; set; }
+    }
+}
---
+++ JobResearchSystem.Application/Feature/Applicants/Commands/Validators/DeleteApplicantlValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using JobResearchSystem.Application.Feature.Applicants.Commands.Models;
+
+namespace JobResearchSystem.Application.Feature.Applicants.Commands.Validators
+{
+    public class DeleteApplicantValidator : AbstractValidator<DeleteApplicantCommand>
+    {
+        public DeleteApplicantValidator() { ApplyValidationsRules(); }
+        public void ApplyValidationsRules()
+        {
+
+            RuleFor(x => x.ApplicantId)
+                .NotEmpty().WithMessage("NotEmpty")
+                .NotNull().WithMessage("Required");
+        }
+    }
+}
